6d2b4f08df0cbd0f6e6ddd77d09f868886def385
Luis Villalobos <luisevm24@outlook.com>
2018-08-16 02:38:46 -0500

    Crear registros de MongoDB con la aplicación web

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 var app = express();
 
 mongoose.connect('mongodb://localhost/web-codigo-facilito-nodejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 var productScheme= {
 	title:String,
@@ -33,6 +37,28 @@ app.get("/",function(req,res){
 	});
 */
 	res.render("index");
+});
+
+app.post("/menu",function(solicitud,respuesta){
+	console.log(solicitud.body);
+
+	if(solicitud.body.password == "123"){
+		var data = {
+			title: solicitud.body.title,
+			description: solicitud.body.description,
+			imageUrl: "/images/taco.png",
+			pricing: solicitud.body.pricing
+		};
+
+		var product = new Product(data);
+		
+		product.save(function(err){
+			console.log(product);
+			respuesta.render("index");
+		});
+	}else{
+		respuesta.render("menu/new");
+	}
 });
 
 app.get("/menu/new", function(solicitud,respuesta){
